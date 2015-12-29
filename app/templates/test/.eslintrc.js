6417e0667f286cea1f759a1d0cf5832cf28a2e21
Adrian Hall <adrian@shellmonger.com>
2015-12-28 17:04:47 -0800

    Fixed dangling comma issue

--- app/templates/test/.eslintrc.js
+++ app/templates/test/.eslintrc.js
@@ -3,7 +3,7 @@ var OFF = 0, WARN = 1, ERROR = 2;
 module.exports = exports = {
     "env": {
 <% if(testFramework==='jasmine') { -%>
-        "jasmine": true
+        "jasmine": true,
 <% } else if(testFramework==='mocha') { -%>
         "mocha": true,
 <% } -%>
